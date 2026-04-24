aa8e4f1f560933e8ff0ff37d1d6f43b450df0e9e
dulmini11 <dulminihw2@gmail.com>
2026-04-24 13:02:36 +0530

    feat: filter and hide extra nodes from chair GLB model

--- src/components/ChairModel.jsx
+++ src/components/ChairModel.jsx
@@ -1,7 +1,38 @@
+import { useEffect } from "react";
 import { useGLTF } from "@react-three/drei";
 
-export default function ChairModel() {
-  const { scene } = useGLTF("/chair_citizen.glb");
+export default function ChairModel({ backType }) {
+  const model = useGLTF("/chair_citizen.glb");
 
-  return <primitive object={scene} scale={1.5} />;
+  useEffect(() => {
+    const hiddenNodes = [
+      "Laser",
+      "Plano",
+      "Cosy_2",
+      "credo",
+      "Laser1",
+      "Plano2",
+      "Cosy_3",
+      "credo1",
+      "Laser2",
+      "Plano3",
+      "Cosy_4",
+      "credo2",
+      "Laser3",
+      "Laser4",
+      "Cosy_5",
+      "credo3",
+      "floor",
+    ];
+
+    hiddenNodes.forEach((name) => {
+      const obj = model.scene.getObjectByName(name);
+      if (obj) {
+        obj.visible = false;
+      }
+    });
+
+  }, [model]);
+
+  return <primitive object={model.scene} scale={1.5} />;
 }
